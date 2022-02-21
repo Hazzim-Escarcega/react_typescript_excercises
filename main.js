2b9e34bb32344aeaffb196d71fcfbfbe35ca31b7
Hazzim-Escarcega <said-hazzim.escarcega@atos.net>
2022-02-21 14:56:00 -0700

    Primitive values vs Reference types

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 //Practice code
+
+/* For primitive types if we assign a primitive value to a variable and the we asign that variable
+to an another variable and the we change the value of the first variable, the second one doesnt
+gets affected because it copies assigned value
+
+let sample = 5;
+let copySample = sample;
+
+sample = 10; */
+
+
+/*For reference types both of the variables get affected because it only copies the memoty reference value of the variable and
+not the value itself*/
+let sample = {name: "Hazzim"};
+let copySample = sample;
+
+sample.lastName = "Escarcega";
+
+console.log("sample: ", sample);
+console.log("copySample: ", copySample);
