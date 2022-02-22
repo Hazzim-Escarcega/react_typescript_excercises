092a830b5fd18035afbc15fe50ef11de278830e6
Hazzim-Escarcega <said-hazzim.escarcega@atos.net>
2022-02-22 10:08:45 -0700

    Spread operator, Rest parameter, Destructuring

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
 //Practice code
+const userDetails = {
+    userName: 'Hazzimov',
+    userDetails: {
+        name: "Hazzim",
+        lastname: "Escarcega",
+        location: {
+            adress: "First street #21",
+            City: "Hidalgo del Parral",
+            state: "Chihuahua",
+            country: "Mexico"
+        }
+
+    }
+}
+
+
+/* ------Rest operator ----------
+function sum(...thisArgs){
+    return thisArgs.reduce(function(previous, current){
+        return previous + current;
+    })
+}
+console.log(sum(5, 5, 5)); 
+
+const newUserDetails = {...userDetails, mobile: '6141234567'}
+console.log("User details: ", userDetails)
+console.log("New user details: ", newUserDetails)
+
+*/
+
+/* -----Destructuring ------- */
+
+const {userName, ...otherDetails} = userDetails;
+
+console.log(userName);
+console.log(otherDetails);
