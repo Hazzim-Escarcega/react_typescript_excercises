fd0f43acf267fb8272017179df77ee77bf608093
Hazzim-Escarcega <said-hazzim.escarcega@atos.net>
2022-02-22 11:10:41 -0700

    arrow  functions

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 //Practice code
+class Printer{
+    name = "I'm a printer";
+    printName(){
+        console.log("Class name: ", this.name);
+    }
+    printNameArrowFunction = () => {
+        console.log("Class name: ", this.name)
+    }
+}
+
+const myPrinter = new Printer();
+/* myPrinter.printName();
+myPrinter.printNameArrowFunction(); */
+
+const customPrinter = {
+    name: "Custom Printer",
+    printName: myPrinter.printName,
+    arrowPrint: myPrinter.printNameArrowFunction
+}
+
+customPrinter.printName();
+customPrinter.arrowPrint();
