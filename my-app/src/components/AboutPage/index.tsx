48fd73b75e0b29ba15048992c4fe5d6f5f782232
Hazzim-Escarcega <said-hazzim.escarcega@atos.net>
2022-03-08 10:38:53 -0700

    Router pt 2

--- my-app/src/components/AboutPage/index.tsx
+++ my-app/src/components/AboutPage/index.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import { AboutPageProps } from './interface';
+import React from "react";
+import { AboutPageProps } from "./interface";
 
 class AboutPage extends React.Component<AboutPageProps> {
-    render() {
-        return (
-            <div>
-                <h1> About Page </h1>
-                <p>Name: {this.props.match.params.username}</p>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1> About Page </h1>
+      </div>
+    );
+  }
 }
 
 export default AboutPage;
